3cd502b22661ebac6743910dec2dd345d6b86d2d
Jonathan <38994980+jonathlan@users.noreply.github.com>
2022-10-14 12:49:24 +0200

    Save some attempts for the donations timing.

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 
 var _endpoint = "https://api.charidy.com/api/v1/campaign/18262/donations?searchQ=&limit=10&sortBy=-time&extend=organization&extend=team&extend=level&extend=converted_currency&extend=campaign"
 
-//_endpoint = 'samples/endpoint.json';
+_endpoint = 'https://jonathlan.github.io/Donations-Banners/demo/endpoint.json';
 
 var _now = new Date(Date.now());
+var _timeout = 5000;
 
 function date_diff(dt2, dt1) {
     var diff = (dt2.getTime() - dt1.getTime()) / 1000;
@@ -21,7 +22,7 @@ async function getDonations(){
     }
     else{
         console.log(`Error fetching data: ${response.status}`)
-        return NaN;
+        return [];
     }
 }
 
@@ -34,15 +35,22 @@ function updateBanner(name, curr, amnt, comment, creationDate){
     document.getElementById("date").innerText = "Hace " + diffminutes + " minutos.";
 }
 
+async function waitfornext(){
+    setTimeout(function() {
+        console.log("Waiting...");
+    }, _timeout)
+}
+
 async function renderBanner(){
     let donations = await getDonations();
 
-    donations.forEach(donation => {        
+    donations.forEach(async donation => {        
         const donAttributes = donation.attributes;
 
         //console.log(`fetch: ${name}, ${comment}, ${creationDate}`);
-
+        await waitfornext();
         updateBanner(donAttributes.name, donAttributes.currency_code, donAttributes.total_charge_currency, donAttributes.dedication, donAttributes.created_at);
+        
     });    
 }
 
