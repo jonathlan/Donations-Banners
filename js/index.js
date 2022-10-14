7d87eb505f550d470ec5c0cec42addfa2fc7010f
jonathlan <38994980+jonathlan@users.noreply.github.com>
2022-10-15 00:38:23 +0200

    Add play, pause, set timer and demo mode controls. Set timer is hidden for now.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,18 @@
-
-var _endpoint = "https://api.charidy.com/api/v1/campaign/18262/donations?searchQ=&limit=10&sortBy=-time&extend=organization&extend=team&extend=level&extend=converted_currency&extend=campaign"
-
-_endpoint = 'https://jonathlan.github.io/Donations-Banners/demo/endpoint.json';
+var _urlProd = "https://api.charidy.com/api/v1/campaign/18262/donations?searchQ=&limit=10&sortBy=-time&extend=organization&extend=team&extend=level&extend=converted_currency&extend=campaign";
+var _urlDemo = "https://jonathlan.github.io/Donations-Banners/demo/endpoint.json";
+var _endpoint = _urlProd;
 
 var _now = new Date(Date.now());
-var _timeout = 5000;
+var _timeout = 8000;
 var _donationsList = [];
 var _k = 0;
+var _intervalID;
 
 function date_diff(dt2, dt1) {
     var diff = (dt2.getTime() - dt1.getTime()) / 1000;
     diff /= 60;
     return Math.abs(Math.round(diff));
 }
-
 
 async function getDonations() {
     const response = await fetch(_endpoint);
@@ -46,20 +45,45 @@ function updateNextDonation() {
     _k++;
 }
 
-function displayHello() {
-    console.log("Hello");
-}
-
 async function renderBanner() {
     let donations = await getDonations();
+    _donationsList.length = 0;
     donations.forEach(donation => {
         _donationsList.push(donation.attributes);
     });
 
-    setInterval(updateNextDonation, _timeout);
+    _intervalID = setInterval(updateNextDonation, _timeout);
 }
 
-renderBanner();
+function playDonations() {
+    document.getElementById("status").innerHTML = "Playing";
+    var isDemo = document.getElementById("btn_demo");
+    if (isDemo.checked) {
+        _endpoint = _urlDemo;
+    }
+    else {
+        _endpoint = _urlProd;
+    }
+    renderBanner();
+    console.log(`Playing. Demo:${isDemo.checked}`);
+}
 
+function pauseDonations() {
+    console.log("Pause.");
+    _donationsList.length = 0;
+    // Clear all intervals.
+    for (let i = 1; i < _intervalID; i++) {
+        window.clearInterval(i);
+    }
+    document.getElementById("status").innerHTML = "Paused";
+}
 
-
+function setTimer() {
+    pauseDonations();
+    var val = document.getElementById("inputSeconds").value;
+    if (val > 0) {
+        _timeout = val * 1000; // Miliseconds
+        console.log(`Using custom timer: ${_timeout}`);
+    }
+    playDonations();
+}
